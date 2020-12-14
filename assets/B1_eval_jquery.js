e5cf3a302dbe849c81216136f983ef1df73f6ae9
Augustin <apetre.pro@gmail.com>
2020-12-14 11:52:24 +0100

    Partie 1 - Q1

--- assets/B1_eval_jquery.js
+++ assets/B1_eval_jquery.js
@@ -3,3 +3,25 @@
 Votre code ici
 
 -------------------------------------------------------------------------- */
+function GameButton() {
+    $(".custom-control-input").click(function (e) { 
+        e.preventDefault();
+        if($("div").hasClass("is-active") == true){
+            $(".redsquare-app").removeClass("is-active");
+            $('button').prop('disabled', true);
+        }
+        else{
+            $(".redsquare-app").addClass("is-active");
+            $("button").removeAttr("disabled");
+        }
+    });
+}
+
+
+
+
+
+
+$(function () {
+    GameButton();
+});
